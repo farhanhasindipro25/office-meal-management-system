97cff85837f7a791634f5210210795788ec02a9f
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-07-12 23:32:08 +0600

    chore: Added database queries regarding items, categories, meals and meal_items

--- server/scripts.sql
+++ server/scripts.sql
@@ -50,3 +50,78 @@ INNER JOIN
 ON 
     users.role_id = roles.id;
 
+
+CREATE TABLE categories (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO categories (name, description)
+VALUES 
+('Starch', 'e.g. Rice, Bread'),
+('Veg', 'e.g. Potato, Tomato, Eggplant'),
+('Protein', 'e.g. Chicken, Beef, Egg, Fish');
+
+
+CREATE TABLE items (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    category_id UUID NOT NULL REFERENCES categories(id),
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+INSERT INTO items (category_id, name) VALUES ('17219629-3cd2-472a-9089-230718669dd3','Plain Rice'),('17219629-3cd2-472a-9089-230718669dd3','Roti'),('364ce12a-1319-4523-9a80-b5e3672801b1','Begun Bhaji'),('364ce12a-1319-4523-9a80-b5e3672801b1','Daal'),('364ce12a-1319-4523-9a80-b5e3672801b1','Potato Bhorta'),('364ce12a-1319-4523-9a80-b5e3672801b1','Tomato Bhorta'),('76c39085-f377-45c0-88cf-31c196e030b3','Fish Curry'),('76c39085-f377-45c0-88cf-31c196e030b3','Chicken Curry'),('76c39085-f377-45c0-88cf-31c196e030b3','Egg Curry'),('76c39085-f377-45c0-88cf-31c196e030b3','Egg Bhorta');
+
+-- Selecting all items with category names
+SELECT 
+    items.id,
+    items.name,
+    categories.name AS category_name,
+    items.created_at,
+    items.updated_at
+FROM 
+    items
+INNER JOIN 
+    categories
+ON 
+    items.category_id = categories.id;
+
+
+CREATE TABLE meals (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  day INTEGER,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO meals (day) VALUES (1),(2),(3),(4),(5);
+
+
+CREATE TABLE meal_items (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  meal_id UUID NOT NULL REFERENCES meals(id),
+  item_id UUID NOT NULL REFERENCES items(id)
+);
+
+INSERT INTO meal_items (meal_id, item_id) VALUES ('f3b3a5ef-62b9-4fbb-a2d2-4588fc4d1044', 'ca36081a-b210-4668-872f-6c908ebfef05');
+
+-- Selecting meal items available per day
+SELECT meals.day AS work_day, STRING_AGG(items.name,',') AS item_name
+FROM meals
+INNER JOIN meal_items ON meals.id = meal_items.meal_id
+INNER JOIN items ON meal_items.item_id = items.id
+GROUP BY meals.day;
+
+
+CREATE TABLE orders (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    meal_id UUID REFERENCES meals(id),
+    wants_meal BOOLEAN,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);

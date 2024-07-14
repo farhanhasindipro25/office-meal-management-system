761fb44b71c1ba9ba45832e38960818e7c528551
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-07-14 23:53:12 +0600

    refactor: Refactored repository export pattern

--- server/src/app/modules/roles/roles.repository.js
+++ server/src/app/modules/roles/roles.repository.js
@@ -1,2 +1,7 @@
-export const POST_ROLE_TO_DB = "INSERT INTO roles (name) VALUES ($1)";
-export const GET_ROLES_FROM_DB = "SELECT * FROM roles";
+const POST_ROLE_TO_DB = "INSERT INTO roles (name) VALUES ($1)";
+const GET_ROLES_FROM_DB = "SELECT * FROM roles";
+
+export const RolesRepository = {
+  POST_ROLE_TO_DB,
+  GET_ROLES_FROM_DB,
+};
--- server/src/app/modules/roles/roles.services.js
+++ server/src/app/modules/roles/roles.services.js
@@ -1,13 +1,13 @@
 import pool from "../../../db.js";
-import { GET_ROLES_FROM_DB, POST_ROLE_TO_DB } from "./roles.repository.js";
+import { RolesRepository } from "./roles.repository.js";
 
 const ADD_ROLE_TO_DB = async (name) => {
-  const response = await pool.query(POST_ROLE_TO_DB, [name]);
+  const response = await pool.query(RolesRepository.POST_ROLE_TO_DB, [name]);
   return response;
 };
 
 const READ_ROLES_FROM_DB = async () => {
-  const response = await pool.query(GET_ROLES_FROM_DB);
+  const response = await pool.query(RolesRepository.GET_ROLES_FROM_DB);
   return response;
 };
 

be57cc347675fc83178905d65d7240ca2a53e1f5
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-07-17 20:13:14 +0600

    feat: Added loading and error reusable components

--- client/src/_libs/components/pages/Admin/user-management/UsersDataList.jsx
+++ client/src/_libs/components/pages/Admin/user-management/UsersDataList.jsx
@@ -1,14 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { getUsersList } from "../../../../services/api/admin/users/getUsersList";
 import UsersListTable from "./UsersListTable";
+import Loader from "../../../ui/Loader";
+import ServerError from "../../../ui/ServerError";
 
 export default function UsersDataList() {
   const { data, isLoading, isError } = useQuery({
     queryKey: ["/users"],
     queryFn: () => getUsersList(),
   });
-  if (isLoading) return "Loading...";
-  if (isError) return "Error...";
+  if (isLoading) return <Loader />;
+  if (isError) return <ServerError />;
 
   return (
     <div className="overflow-x-auto">
---
+++ client/src/_libs/components/ui/ServerError.jsx
@@ -0,0 +1,7 @@
+export default function ServerError() {
+  return (
+    <div className="w-full rounded-md border border-red-300 bg-red-50 flex justify-center items-center h-screen text-sm font-medium text-red-500">
+      The server ran into an issue! Please try again later.
+    </div>
+  );
+}
